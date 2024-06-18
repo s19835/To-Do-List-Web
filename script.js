cda579c8d3430650b6d73142e7fed00dfe8609cd
s19835 <s19835@sci.pdn.ac.lk>
2024-06-18 15:53:15 +0530

    archive list added

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById("todolist");
     const input = document.getElementById("task");
-    const todolist = document.getElementById("todotasks");
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        addTask();
+        addTask(input.value);
+        input.value = '';
     })
 });
 
-function addTask() {
-    alert("task added");
+function addTask(task) {
+    const todolist = document.getElementById("todotasks");
+    const archive = document.getElementById("archive");
+    const list = document.createElement('li');
+    list.innerText = task;
+
+    const checkButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+
+    checkButton.textContent = '✔';
+    checkButton.addEventListener('click', () => {
+        list.classList.toggle('completed');
+        const archiveList = document.createElement('li');
+        archiveList.textContent = task;
+        archive.appendChild(archiveList);
+        todolist.removeChild(list);
+    });
+
+    deleteButton.textContent = '✘'
+    deleteButton.addEventListener('click', () => {
+        todolist.removeChild(list);
+    })
+
+
+    list.appendChild(checkButton);
+    list.appendChild(deleteButton);
+    todolist.appendChild(list);
 }
